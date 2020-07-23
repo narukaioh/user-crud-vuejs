f015e19ab7f7a648071aba933f0f850ff0bd0c0e
Ju Dantas <jucienyds@gmail.com>
2020-07-23 01:02:27 -0300

    added login fetch

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,22 +1,81 @@
 <template>
   <form class="login-form">
     <span>Email:</span>
-    <input type="email" />
+    <input type="email" v-model="user.email" />
 
     <span>Password:</span>
-    <input type="password" />
+    <input type="password" v-model="user.password" />
 
-    <button-options />
+    <div class="button-options">
+      <button-primary
+        label="Enviar"
+        :callback="signin(user)" />
+
+      <button-secondary
+        label="Limpar"
+        :callback="clear(user)" />
+    </div>
+
+    <router-link to="/signup">Signup</router-link>
   </form>
 </template>
 
 <script>
-import ButtonOptions from '@/components/toolbar/ButtonOptions'
+import { login } from '@/services/user'
+import ButtonPrimary from '@/components/buttons/ButtonPrimary'
+import ButtonSecondary from '@/components/buttons/ButtonSecondary'
+import { mapActions, mapState } from 'vuex'
 
 export default {
   name: 'Login',
   components: {
-    ButtonOptions
+    ButtonPrimary,
+    ButtonSecondary
+  },
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  computed: {
+    ...mapState({
+      isLogin: state => state.token !== ''
+    })
+  },
+  methods: {
+    ...mapActions(['login', 'logout']),
+
+    clear (user) {
+      return () => {
+        console.log(user.email)
+      }
+    },
+
+    signout () {
+      this.logout()
+    },
+
+    signin (user) {
+      return () => {
+        login(user).then(res => {
+          if (res.user) {
+            const { name, email } = res.user
+            const token = res.token
+
+            this.login({
+              user: { name, email },
+              token
+            })
+            this.$router.push('/users')
+          } else {
+            console.log(new Error('problema com login'))
+          }
+        })
+      }
+    }
   }
 }
 </script>
